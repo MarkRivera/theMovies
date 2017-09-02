486914273c2f92770b6c6e6d335f5b04fbbfda51
Mark <Mrivera1991@gmail.com>
2017-09-02 17:38:14 -0400

    Updated code
    Left in my old code to see the differences between my old ideas. Found a quicker and more elegant way to implement autocomplete with jquery.

--- Movie Project/java.js
+++ Movie Project/java.js
@@ -1,7 +1,7 @@
 $( document ).ready(function() {
     console.log( "ready!" );
     var imdbUrl = "https://api.themoviedb.org/3/search/movie?api_key=a6a4e825f844dc112dab513389c719fe";
-    var searchBar = $("#movie_search_bar");
+    var $searchBar = $("#movie_search_bar");
     var userInput;
     
     var searchResults;
@@ -10,32 +10,89 @@ $( document ).ready(function() {
     var movieDescription;
     
     //input functionality
-    
-    searchBar.keyup(function(){
-        userInput = searchBar.val();
-        setTimeout(function(){
-            if (userInput.length >= 1) {
-                console.log("I'm here!");
-                ajaxMovieTitlesForAutofill(imdbUrl, "GET");    
-            }
-        },1000);
+    $searchBar.keypress(function(){
+        userInput = $searchBar.val();
     });
     
-    function ajaxMovieTitlesForAutofill(url, methodType){
-        
-        $.ajax({
-            url: url,
-            data: "query="+userInput,
-            type: methodType,
-            success: function(result) {
-            //save results in a more accessible variable.
-                searchResults = result;
-                console.log(searchResults);
-            }
-        });
-    }
     
+    $searchBar.autocomplete({
+        minLength: 2,
+        source: function (request, response) {
+            $.ajax({
+              url: imdbUrl,
+              data: "query="+userInput,
+              type: "GET",
+              success: function (result) {
+                  var titles = [];
+                  for (i = 0; i < 10; i++) {
+                      titles.push(result.results[i].title);
+                  }
+                  response(titles);
+              }
+            });
+        }
+    });
     
+//    $searchBar.keypress(function(){
+//        userInput = $searchBar.val();
+//        setTimeout(function(){
+//            if (userInput.length >= 1) {
+//                console.log("I'm here!");
+//                ajaxMovieTitlesForAutofill(imdbUrl, "GET");    
+//            }
+//        },1000);
+//    });
+    
+//    function ajaxMovieTitlesForAutofill(url, methodType){
+//        
+//        $.ajax({
+//            url: url,
+//            data: "query="+userInput,
+//            type: methodType,
+//            success: function(result) {
+//            //save results in a more accessible variable.
+//                searchResults = result;
+//                getTenSuggestions (0);
+//                console.log(searchResults);
+//            }
+//        });
+//    }
+//    
+//    
+//    function getTenSuggestions (n){
+//        var titleArr = [];
+//        
+//        for ( n; n < 10; n++ ) {
+//            titleArr.push(searchResults.results[n].title);
+//        }
+//        console.log(titleArr);
+//        createUniqueTitleArray(titleArr);
+//    };
+//    
+//    function createUniqueTitleArray (titleArr) {
+//      var uniqueTitles = titleArr.filter(function(item, index, title){
+//          return index == title.indexOf(item);
+//      });  
+//        createAutocomplete(uniqueTitles);
+//    };
+//    
+//    function createAutocomplete (uniqueTitles) {
+//      $("#movie_search_bar").autocomplete({
+//       source: uniqueTitles
+//    });  
+//    };
+    
+//    function placeTenSuggestionsInAutoFill (titleArr) {
+//        var uniqueTitles = titleArr.filter(function(item, index, title){
+//            return index == title.indexOf(item);
+//        });
+//        $.each(uniqueTitles, function(index, value){
+//            $('<option></option>', {
+//                id: "movies",
+//                value: value,
+//                           }).appendTo('#movies');
+//        });
+//    }
     //Async call for movie data
 //    $.ajax({
 //        url: imdbUrl,
